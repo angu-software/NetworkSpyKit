43a52a5f21d620a8e1c5b8a58db0ef150ec3af98
angu-software <>
2025-06-15 21:46:38 +0200

    TIDY Interceptor

--- Sources/NetworkSpyKit/InterceptorURLProtocol.swift
+++ Sources/NetworkSpyKit/InterceptorURLProtocol.swift
@@ -16,12 +16,44 @@ final class InterceptorURLProtocol: URLProtocol {
 
     var spyRegistry: SpyRegistry = .shared
 
+    private func response(for request: URLRequest) throws -> (response: HTTPURLResponse, data: Data?) {
+        let spy = try spy(for: request)
+        return try spy.response(for: request)
+    }
+
     private func spy(for request: URLRequest) throws -> NetworkSpy {
         guard let spy = spyRegistry.spy(for: request) else {
             throw Error.spyNotFoundForRequest
         }
 
         return spy
+    }
+
+    private func respond(with response: (response: HTTPURLResponse, data: Data?)) {
+        respond(with: response.response)
+
+        if let responseData = response.data {
+            respond(with: responseData)
+        }
+    }
+
+    private func respond(with responseData: Data) {
+        client?.urlProtocol(self,
+                            didLoad: responseData)
+    }
+
+    private func respond(with response: HTTPURLResponse) {
+        client?.urlProtocol(self,
+                            didReceive: response,
+                            cacheStoragePolicy: .notAllowed)
+    }
+
+    private func respond(with error: any Swift.Error) {
+        client?.urlProtocol(self, didFailWithError: error)
+    }
+
+    private func finishLoading() {
+        client?.urlProtocolDidFinishLoading(self)
     }
 
     // MARK: - URLProtocol
@@ -34,24 +66,15 @@ final class InterceptorURLProtocol: URLProtocol {
         // TODO: canonicazor to sort query parameters for reliable comparison
         return request
     }
-
+    
     override func startLoading() {
         do {
-            let spy = try spy(for: request)
-            let response = try spy.response(for: request)
-            client?.urlProtocol(self,
-                                didReceive: response.response,
-                                cacheStoragePolicy: .notAllowed)
-
-            if let responseData = response.data {
-                client?.urlProtocol(self,
-                                    didLoad: responseData)
-            }
+            respond(with: try response(for: request))
         } catch {
-            client?.urlProtocol(self, didFailWithError: error)
+            respond(with: error)
         }
 
-        client?.urlProtocolDidFinishLoading(self)
+        finishLoading()
     }
 
     override func stopLoading() {

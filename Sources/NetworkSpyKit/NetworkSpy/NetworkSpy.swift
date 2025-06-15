14317c43caeabefb8a7d007846b0309e7e0510de
angu-software <>
2025-06-15 22:57:42 +0200

    ADD setting responseProvider after spy initialization (thread safe)

--- Sources/NetworkSpyKit/NetworkSpy/NetworkSpy.swift
+++ Sources/NetworkSpyKit/NetworkSpy/NetworkSpy.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class NetworkSpy: Sendable {
+public final class NetworkSpy: @unchecked Sendable {
 
     public typealias ResponseProvider = @Sendable (Request) throws -> StubbedResponse
 
@@ -15,12 +15,26 @@ public final class NetworkSpy: Sendable {
     ///
     /// A copy of the injected `URLSessionConfiguration` instance during initialization
     public let sessionConfiguration: URLSessionConfiguration
+    public var responseProvider: ResponseProvider {
+        get {
+            queue.sync {
+                return self._responseProvider
+            }
+        }
+        set {
+            queue.async(flags: .barrier) {
+                self._responseProvider = newValue
+            }
+        }
+    }
 
     static let headerKey = "X-NetworkSpy-ID"
     let id: String = UUID().uuidString
-    let responseProvider: ResponseProvider
 
     private let spyRegistry: SpyRegistry
+    private let queue: DispatchQueue = DispatchQueue(label: "\(NetworkSpy.self)-\(UUID().uuidString)",
+                                                     attributes: .concurrent)
+    private var _responseProvider: ResponseProvider
 
     // TODO: tell that the injected session is a "template"
     public convenience init(sessionConfiguration: URLSessionConfiguration,
@@ -29,14 +43,14 @@ public final class NetworkSpy: Sendable {
                   responseProvider: responseProvider,
                   spyRegistry: .shared)
     }
-
+    
     init(sessionConfiguration: URLSessionConfiguration,
-                responseProvider: @escaping ResponseProvider,
-                spyRegistry: SpyRegistry) {
+         responseProvider: @escaping ResponseProvider,
+         spyRegistry: SpyRegistry) {
         self.sessionConfiguration = Self.copyConfiguration(sessionConfiguration)
-        self.responseProvider = responseProvider
+        self._responseProvider = responseProvider
         self.spyRegistry = spyRegistry
-
+        
         setUp()
     }
 
--- Tests/NetworkSpyKitTests/NetworkSpyKitTests.swift
+++ Tests/NetworkSpyKitTests/NetworkSpyKitTests.swift
@@ -1,7 +1,16 @@
+//
+//  NetworkSpyKitTests.swift
+//  NetworkSpyKit
+//
+//  Created by Andreas Günther on 13.06.25.
+//
+
+import Foundation
 import Testing
 
 import NetworkSpyKit
 
+// Public API tests
 struct NetworkSpyKitTests {
 
     private typealias Response = NetworkSpy.StubbedResponse
@@ -23,7 +32,36 @@ struct NetworkSpyKitTests {
                                              data: "Hello spy!".data(using: .utf8)))
     }
 
-    // TODO: throwing responseProvider
-    // TODO: load data
-    // TODO: change provider after initialization
+    @Test
+    func should_fail_sending_request_when_response_provider_throws_error() async throws {
+        let spy = NetworkSpy(sessionConfiguration: .default) { _ in
+            throw TestingError.somethingWentWrong
+        }
+
+        let networkClient = NetworkClientFake(sessionConfiguration: spy.sessionConfiguration)
+
+        do {
+            _ = try await networkClient.sendRequest()
+            Issue.record("Request should have failed, but did not!")
+        } catch {
+            let error = error as NSError
+            #expect(error.domain == String(reflecting: TestingError.self))
+            #expect(error.code == 0)
+        }
+    }
+
+    @Test
+    func should_allow_changing_response_provider_after_initalization() async throws {
+        let spy = NetworkSpy(sessionConfiguration: .default) { _ in
+            throw TestingError.somethingWentWrong
+        }
+        let networkClient = NetworkClientFake(sessionConfiguration: spy.sessionConfiguration)
+        spy.responseProvider = { _ in return .teaPot }
+
+        let response = try await networkClient.sendRequest()
+
+        #expect(response == .teaPot)
+    }
+
+    // Default response provider
 }

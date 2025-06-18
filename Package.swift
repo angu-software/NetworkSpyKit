0b112d224543d679289fd62e4523662f5c7c8532
angu-software <>
2025-06-18 08:28:32 +0200

    MOD enable swift 6 strict concurrency checks for development

--- Package.swift
+++ Package.swift
@@ -12,7 +12,14 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "NetworkSpyKit"),
+            name: "NetworkSpyKit",
+            swiftSettings: [
+                // Opt into Swift 6 strict concurrency checks during development
+                .unsafeFlags(["-Xfrontend", "-enable-actor-data-race-checks",
+                              "-Xfrontend", "-strict-concurrency=complete",
+                              "-warnings-as-errors"],
+                             .when(configuration: .debug))
+            ]),
         .testTarget(
             name: "NetworkSpyKitTests",
             dependencies: ["NetworkSpyKit"]

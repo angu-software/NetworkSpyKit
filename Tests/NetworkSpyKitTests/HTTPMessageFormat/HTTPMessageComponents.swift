a6497355ff86df512faf3b83bca25c51abaf1d2c
Andreas Günther <a.guenther@dccx.it>
2025-10-30 08:53:42 +0100

    feat(HTTPMessageComponents_RFC-9112): Given header fields format includes fields lines

--- Tests/NetworkSpyKitTests/HTTPMessageFormat/HTTPMessageComponents.swift
+++ Tests/NetworkSpyKitTests/HTTPMessageFormat/HTTPMessageComponents.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Testing
 
 struct HTTPMessageComponents {
 
@@ -25,39 +26,90 @@ struct HTTPMessageComponents {
 
     // message-body = *OCTET
 
-    // origin-form = absolute-path [ "?" query ]
-    // absolute-path = <absolute-path, see [HTTP], Section 4.1>
-    // query = <query, see [URI], Section 3.4>
-
     var url: URL?
+    var headerFields: [String: String]?
+
+    private let newLine = "\n"
 
     func httpMessageFormat() -> String {
+        return [
+            requestTarget(),
+            fieldLines(),
+        ]
+        .compactMap { $0 }
+        .joined(separator: newLine)
+    }
+
+    private func requestTarget() -> String? {
+        // request-target = origin-form / absolute-form / authority-form / asterisk-form
+        // origin-form = absolute-path [ "?" query ]
+        // ✅ absolute-path = <absolute-path, see [HTTP], Section 4.1>
+        // query = <query, see [URI], Section 3.4>
         guard let url else {
-            return ""
+            return nil
         }
 
         return url.path
     }
+
+    private func fieldLines() -> String? {
+        guard let headerFields,
+            headerFields.isEmpty == false
+        else {
+            return nil
+        }
+
+        return
+            headerFields
+            .map { "\($0.key): \($0.value)" }
+            .sorted()
+            .joined(separator: newLine)
+    }
 }
 
-import Testing
-
-struct HTTPMessageComponentsTests {
+struct HTTPMessageComponentsFormattingTests {
 
     private let url = URL(string: "https://www.rfc-editor.org/rfc/rfc9112.pdf")
+    private let headerFields = [
+        "User-Agent": "NetworkSpyKit/1.0",
+        "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.9",
+    ]
 
     @Test
-    func givenNothing_itBuildsEmptyString() {
+    func givenNothing_itReturnsEmptyString() {
         let components = HTTPMessageComponents()
 
         #expect(components.httpMessageFormat().isEmpty)
     }
 
+    // MARK: request-target: origin-form
+
     @Test
-    func givenURL_itContainsAbsolutePath() {
+    func givenURL_itIncludesAbsolutePath() {
         var components = HTTPMessageComponents()
         components.url = url
 
-        #expect(components.httpMessageFormat() == "/rfc/rfc9112.pdf")
+        #expect(
+            components.httpMessageFormat() == """
+                /rfc/rfc9112.pdf
+                """
+        )
+    }
+
+    @Test
+    func givenHeaderFields_itIncludesFieldLines() {
+        var components = HTTPMessageComponents()
+        components.url = url
+        components.headerFields = headerFields
+
+        #expect(
+            components.httpMessageFormat() == """
+                /rfc/rfc9112.pdf
+                Accept-Language: en-US,en;q=0.9
+                Accept: application/json
+                User-Agent: NetworkSpyKit/1.0
+                """
+        )
     }
 }

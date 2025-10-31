19a80bf657702855b9e441b820bb044d2dfa5b34
Andreas Günther <a.guenther@dccx.it>
2025-10-31 07:08:58 +0100

    refactor(HTTPMessageComponnent_RFC-9112): Move formatting of start line from HTTPMessageComponents.StartLine to formatter

--- Sources/NetworkSpyKit/HTTPMessageComponents/HTTPMessageComponents-StartLine.swift
+++ Sources/NetworkSpyKit/HTTPMessageComponents/HTTPMessageComponents-StartLine.swift
@@ -10,7 +10,6 @@ import Foundation
 extension HTTPMessageComponents {
 
     struct StartLine {
-        private let space = " "
 
         // request-line
         var method: String?
@@ -21,90 +20,5 @@ extension HTTPMessageComponents {
         var statusReason: String?
 
         var httpVersion: String?
-
-        var isStatusLine: Bool {
-            return statusCode != nil
-        }
-
-        // ✅ start-line = request-line / status-line
-        func format() -> String? {
-            if isStatusLine {
-                return statusLine()
-            }
-
-            return requestLine()
-        }
-
-        // ✅ request-line = method SP request-target SP HTTP-version
-        private func requestLine() -> String? {
-            return [
-                methodToken(),
-                requestTarget(),
-                httpVersionToken(),
-            ]
-                .compactMap { $0 }
-                .joined(separator: space)
-        }
-
-        // request-target = origin-form / absolute-form / authority-form / asterisk-form
-        // ✅ origin-form = absolute-path [ "?" query ]
-        // ✅ absolute-path = <absolute-path, see [HTTP], Section 4.1>
-        // ✅ query = <query, see [URI], Section 3.4>
-        private func requestTarget() -> String? {
-            guard let absolutePath else {
-                return nil
-            }
-
-            return absolutePath.isEmpty ? "/" : absolutePath
-        }
-
-        // method = token
-        private func methodToken() -> String? {
-
-            guard let method else {
-                return nil
-            }
-
-            return method.uppercased()
-        }
-
-        // status-line = HTTP-version SP status-code SP [ reason-phrase ]
-        private func statusLine() -> String? {
-            return [
-                httpVersionToken(),
-                statusCodeToken(),
-                statusReasonPhrase(),
-            ]
-                .compactMap { $0 }
-                .joined(separator: space)
-        }
-
-        // status-code = 3DIGIT
-        private func statusCodeToken() -> String? {
-            guard let statusCode else {
-                return nil
-            }
-
-            return "\(statusCode)"
-        }
-
-        // reason-phrase = 1*( HTAB / SP / VCHAR / obs-text )
-        private func statusReasonPhrase() -> String? {
-            guard let statusReason else {
-                return nil
-            }
-
-            return statusReason
-        }
-
-        // HTTP-version = HTTP-name "/" DIGIT "." DIGIT
-        // HTTP-name = %x48.54.54.50 ; HTTP
-        private func httpVersionToken() -> String? {
-            guard let httpVersion else {
-                return nil
-            }
-
-            return httpVersion
-        }
     }
 }
--- Sources/NetworkSpyKit/HTTPMessageComponents/HTTPMessageFormatter.swift
+++ Sources/NetworkSpyKit/HTTPMessageComponents/HTTPMessageFormatter.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct HTTPMessageFormatter {
 
+    private let space = " "
     private let newLine = "\n"
     private var startLine = HTTPMessageComponents.StartLine()
 
@@ -17,7 +18,7 @@ struct HTTPMessageFormatter {
     }
 
     // ✅ HTTP-message = start-line CRLF *( field-line CRLF ) CRLF [ message-body ]
-    func makeHTTPMessageFormat(components: HTTPMessageComponents) -> String {
+    private func makeHTTPMessageFormat(components: HTTPMessageComponents) -> String {
         return [
             makeStartLine(components: components),
             makeFieldLines(components: components),
@@ -25,10 +26,6 @@ struct HTTPMessageFormatter {
         ]
         .compactMap { $0 }
         .joined(separator: newLine)
-    }
-
-    private func makeStartLine(components: HTTPMessageComponents) -> String? {
-        return components.startLine.format()
     }
 
     // ✅ field-line = field-name ":" OWS field-value OWS
@@ -61,5 +58,90 @@ struct HTTPMessageFormatter {
             bodyString,
         ]
         .joined()
+    }
+
+    private func isStatusLine(components: HTTPMessageComponents) -> Bool {
+        return components.startLine.statusCode != nil
+    }
+
+    // ✅ start-line = request-line / status-line
+    private func makeStartLine(components: HTTPMessageComponents) -> String? {
+        if isStatusLine(components: components) {
+            return makeStatusLine(components: components)
+        }
+
+        return makeRequestLine(components: components)
+    }
+
+    // ✅ request-line = method SP request-target SP HTTP-version
+    private func makeRequestLine(components: HTTPMessageComponents) -> String? {
+        return [
+            makeMethodToken(components: components),
+            makeRequestTarget(components: components),
+            makeHTTPVersionToken(components: components),
+        ]
+            .compactMap { $0 }
+            .joined(separator: space)
+    }
+
+    // request-target = origin-form / absolute-form / authority-form / asterisk-form
+    // ✅ origin-form = absolute-path [ "?" query ]
+    // ✅ absolute-path = <absolute-path, see [HTTP], Section 4.1>
+    // ✅ query = <query, see [URI], Section 3.4>
+    private func makeRequestTarget(components: HTTPMessageComponents) -> String? {
+        guard let absolutePath = components.absolutePath else {
+            return nil
+        }
+
+        return absolutePath.isEmpty ? "/" : absolutePath
+    }
+
+    // method = token
+    private func makeMethodToken(components: HTTPMessageComponents) -> String? {
+
+        guard let method = components.method else {
+            return nil
+        }
+
+        return method.uppercased()
+    }
+
+    // status-line = HTTP-version SP status-code SP [ reason-phrase ]
+    private func makeStatusLine(components: HTTPMessageComponents) -> String? {
+        return [
+            makeHTTPVersionToken(components: components),
+            makeStatusCodeToken(components: components),
+            makeStatusReasonPhrase(components: components),
+        ]
+            .compactMap { $0 }
+            .joined(separator: space)
+    }
+
+    // status-code = 3DIGIT
+    private func makeStatusCodeToken(components: HTTPMessageComponents) -> String? {
+        guard let statusCode = components.statusCode else {
+            return nil
+        }
+
+        return "\(statusCode)"
+    }
+
+    // reason-phrase = 1*( HTAB / SP / VCHAR / obs-text )
+    private func makeStatusReasonPhrase(components: HTTPMessageComponents) -> String? {
+        guard let statusReason = components.statusReason else {
+            return nil
+        }
+
+        return statusReason
+    }
+
+    // HTTP-version = HTTP-name "/" DIGIT "." DIGIT
+    // HTTP-name = %x48.54.54.50 ; HTTP
+    private func makeHTTPVersionToken(components: HTTPMessageComponents) -> String? {
+        guard let httpVersion = components.httpVersion else {
+            return nil
+        }
+
+        return httpVersion
     }
 }
